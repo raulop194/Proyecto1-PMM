9071e63f5573c26bb4129d3a0725b64938d4a924
raulop194 <raulop194@alu.centroafuera.es>
2022-11-17 14:16:29 +0100

    commit 17/11: finished exercise 2

--- app/src/main/java/es/nexcreep/testing/proyecto1/MainActivity.kt
+++ app/src/main/java/es/nexcreep/testing/proyecto1/MainActivity.kt
@@ -2,7 +2,13 @@ package es.nexcreep.testing.proyecto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     private val onCreate = 0
@@ -13,11 +19,18 @@ class MainActivity : AppCompatActivity() {
     private val onRestart = 5
     private val onDestroy = 6
 
+    private lateinit var textView: TextView
+    private lateinit var editText: EditText
+    private lateinit var editTextB: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("eventWatcher", "onCreate $onCreate")
+
+        editText = findViewById(R.id.edit_text_1)
+        editTextB = findViewById(R.id.edit_text_2)
     }
 
     override fun onStart() {
@@ -48,5 +61,27 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d("eventWatcher", "onDestroy $onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("edit1", editText.text.toString())
+        outState.putString("edit2", editTextB.text.toString())
+
+        super.onSaveInstanceState(outState)
+        Log.d("eventWatcher", "Saved instance")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        var text = savedInstanceState.getString("edit1")
+        if (text != null)
+            editText.setText(text)
+
+        text = savedInstanceState.getString("edit2")
+        if (text != null)
+            editTextB.setText(text)
+
+        Log.d("eventWatcher", "Restored instance")
     }
 }
